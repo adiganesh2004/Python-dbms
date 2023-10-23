49a5e6848ebbcb100102bba7e292328a4f0eb332
adiganesh2004 <127213712+adiganesh2004@users.noreply.github.com>
2023-10-23 18:58:14 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,24 @@
+import json
+import os
+import csv
+
+data ={"emp_details":[{'Name': 'Alex', 'dept_id': 6, 'emp_id': 1},
+    {'Name': 'Brad', 'dept_id': 4, 'emp_id': 2},
+    {'Name': 'Joey', 'dept_id': 5, 'emp_id': 3},
+    {'Name': 'Peter', 'dept_id': 3, 'emp_id': 4},
+    {'Name': 'Magnus', 'dept_id': 4, 'emp_id': 5}]}
+with open("database.json", "w") as csvfile:
+        json.dump( data,csvfile)
+"""fieldnames = ['Name', 'dept_id', 'emp_id'] 
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(y)
+
+"""
+with open("database.json","r") as reader:
+    var=json.load(reader)
+
+for i in var['emp_details']:
+    print(i)
+
+reader.close
